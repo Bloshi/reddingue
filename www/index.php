624ffd708accaf643ca978f3845c3f35815f577f
poulycroc <esgalduine@gmail.com>
2017-03-19 21:49:07 +0100

    finalisation des bases | css | js

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <?php require_once 'parts/meta.php'; ?>
+        <title>redding</title>
+    </head>
+    <body>
+        
+        
+
+        <?php include_once 'parts/footer.php'; ?>
+        <script src="assets/js/app.js"></script>
+    </body>
+</html>
---
+++ www/parts/meta.php
@@ -0,0 +1,3 @@
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="assets/css/app.css" />
